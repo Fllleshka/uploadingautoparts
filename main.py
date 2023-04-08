66bdf00287733039d57837045bfe629c97153421
Fllleshka <fleisner.omsk@gmail.com>
2023-04-08 16:36:30 +0600

    Добавлена функция скриншота рабочего стола, для проверки работы

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
+import os
 import time
 import pyautogui
 import datetime
+import pyautogui
 
 # Включение паузы 1.5 секунды между действиями
 pyautogui.PAUSE = 1
 
 # Функция перемещения мышки и кликов
-def movemouse(massx, massy):
+def movemouse(massx, massy, numberbutton):
     for elem in range(0, len(massx)):
         print(f'{elem + 1}\t\t[{massx[elem]}]\t[{massy[elem]}]')
         pyautogui.moveTo(massx[elem], massy[elem], duration=0.25)
-        pyautogui.leftClick()
+        # Тип скриншота кнопка
+        if elem == numberbutton:
+            pyautogui.leftClick()
+            # reg = (100-20, 270-10, 60, 20)
+            reg = (0, 0, 1920, 1080)
+            takeascreenshot("Button", "screenshots/proofofwork/", reg)
+        else:
+            pyautogui.leftClick()
+
+
+# Функция скриншота
+def takeascreenshot(name, path, reg):
+    # Время сейчас
+    today = datetime.datetime.today()
+    todaytime = today.strftime("%d.%m.%Y.%H.%M.%S")
+    path = path + name + " " + todaytime + ".png"
+    screen = pyautogui.screenshot(path, region = reg)
 
 # Функция выбора действия от времени
 def switcher(argument):
     match argument:
         # Время сканирования папки
+        # "Проставить остатки"
         case times.putDownBalances:
             # [0] Перемещение к свёрнотому рабочему столу
             # [1] Перемещение к вкладке "Проставить остатки"
@@ -23,7 +42,10 @@ def switcher(argument):
             # [3] # Перемещение к сворачиванию удалённого рабочего стола
             massx = [260, 960, 200, 100, 1160]
             massy = [1060, 540, 1005, 270, 10]
-            movemouse(massx, massy)
+            movemouse(massx, massy, 3)
+            #reg = (1800, 1040, 70, 40)
+            #takeascreenshot("Date&Time", "screenshots/", reg)
+        # "Выгрузка товаров на сайт"
         case times.uploadingpricetoweb:
             # [0] Перемещение к свёрнотому рабочему столу
             # [1] Перемещение к вкладке "Выгрузка товаров на сайт"
@@ -31,14 +53,16 @@ def switcher(argument):
             # [3] # Перемещение к сворачиванию удалённого рабочего стола
             massx = [260, 960, 400, 100, 1160]
             massy = [1060, 540, 1005, 300, 10]
-            movemouse(massx, massy)
+            movemouse(massx, massy, 3)
+        # Подтверждение новой даты
         case times.acceptnewday:
             # [0] Перемещение к свёрнотому рабочему столу
             # [1] Перемещение к кнопке "Сменить рабочую дату"
             # [2] # Перемещение к сворачиванию удалённого рабочего стола
             massx = [260, 960, 920, 1160]
             massy = [1060, 540, 570, 10]
-            movemouse(massx, massy)
+            movemouse(massx, massy, 3)
+        # Закрытие таблицы с проставлением остатков
         case times.closetable:
             # [0] Перемещение к свёрнотому рабочему столу
             # [1] Перемещение к вкладке "Сформировать2"
@@ -46,7 +70,7 @@ def switcher(argument):
             # [3] # Перемещение к сворачиванию удалённого рабочего стола
             massx = [260, 960, 800, 1910, 1160]
             massy = [1060, 540, 1005, 25, 10]
-            movemouse(massx, massy)
+            movemouse(massx, massy, 3)
         case default:
             return print("Время сейчас:\t",argument)
 
@@ -65,10 +89,10 @@ class times:
     uploadingpricetoweb = datetime.time(22, 20).strftime("%H:%M")
     # Время для закрытия таблицы с проставлением остатков
     #closetable = today.time().strftime("%H:%M")
-    closetable = datetime.time(22, 10).strftime("%H:%M")
+    closetable = datetime.time(23, 0).strftime("%H:%M")
     # Время для подтверждения новой даты
     #acceptnewday = today.time().strftime("%H:%M")
-    acceptnewday = datetime.time(3, 0).strftime("%H:%M")
+    acceptnewday = datetime.time(7, 0).strftime("%H:%M")
 
 # Вечный цикл с таймером 0.5 секунд
 while True:
